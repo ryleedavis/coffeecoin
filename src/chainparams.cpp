21a12bbc4bf5d417a0eb1f7c2744552039ca7873
Rylee Davis <ryleedavis@pop-os.localdomain>
2021-09-14 14:24:46 -0600

    setting total coin

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -126,8 +126,8 @@ public:
         //vSeeds.emplace_back("dnsseed.Coffeecointools.com");
         //vSeeds.emplace_back("dnsseed.Coffeecoinpool.org");
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,45);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,48);
         base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,50);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,176);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
