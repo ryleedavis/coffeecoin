11e0007e33ac01de9dcbfe40af7425742ef225be
Rylee Davis <ryleedavis@pop-os.localdomain>
2021-09-15 10:34:46 -0600

    Coin amount

--- src/validation.cpp
+++ src/validation.cpp
@@ -1158,7 +1158,7 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 952.224 * COIN;
+    CAmount nSubsidy = 952.2236655* COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
